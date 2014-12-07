34444610a91e3d27a98443984eba7fdeb05ee1e6
stokefairyjay <jonathan.jefferies@gmail.com>
2014-12-07 10:26:03 +0000

    Create Report.php

---
+++ Report.php
@@ -0,0 +1,132 @@
+<html>
+
+<head>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="./style.css"> 
+
+</head> 
+
+<body>
+
+<a href="report.php?id=164">report a</a>
+
+            <table align="center"> 
+
+                <tr>
+
+                    <th colspan="2"><h2>Choose a location you would like a report for</h2></th>
+
+                </tr>
+
+                <tr>
+
+                    <td><a href="report.php?id=164" class="black">Exmouth</a></td>
+
+                </tr>
+
+                <tr>   
+
+                    <td><a href="report.php?id=6" class="black">Bude</a></td>
+
+                </tr>
+
+                <tr>
+
+                   <td><a href="report.php?id=164" class="black">Newquay</a></td>
+
+                </tr>
+
+                <tr>
+
+                    <td><a href="report.php?id=7" class="black">Croyde</a></td>
+
+                </tr>
+
+                <tr>    
+
+                    <td><a href="report.php?id=10" class="black">Bantham</a></td>
+
+                </tr>    
+
+            </table>  
+
+  <?php   
+
+session_start();
+
+	$spotId = $_GET["id"];
+
+	if(isset($spotId) && $spotId > 0){ 		
+			
+
+		require('MSWFacade.php');
+
+		$MSWFacade = new MSWFacade(); 
+
+		$response = $MSWFacade->getMSWReportAsAssocArray($spotId);
+
+		
+
+		/*
+
+		include_once('dBug.php');
+
+		new dBug($response);
+
+		*/
+
+			
+
+		echo(" <table class='surfreport'>
+
+	  <tr>
+
+		<th>Date</th>
+
+		<th>Swell</th>
+
+		<th>Period</th>
+
+		<th>Wind</th>
+
+		<th>Swell Chart</th>
+
+	  </tr>");
+
+		for ($i = 0; $i < sizeof($response); $i++)
+
+		{
+
+			$date = date("l d/m/y",$response[$i]['localTimestamp']);
+
+			$swell = $response[$i]['swell']['components']['combined']['height'];
+
+			$period = $response[$i]['swell']['components']['combined']['period'];
+
+			$windspeed = $response[$i]['wind']['speed'];
+
+			$windDirection = $response[$i]['wind']['compassDirection'];
+
+			$fadedRating = $response[$i]['fadedRating'];
+
+			$solidRating = $response[$i]['solidRating'];
+
+			$swellChart = $response[$i]['charts']['swell'];
+
+			echo("<tr " . $trclass . ">" . "<td>" . $date . "</td> <td>" . $swell . " foot"."</td> <td>" . $period . " seconds </td> <td>" .$windspeed . " mph<br>" . $windDirection . "</td> <td> <img src='" . $swellChart ."' alt='swell chart'> </td> </tr>");
+
+		}
+
+		echo("</table>");
+
+
+	}
+
+  ?>
+
+
+ 	</body> 
+
+</html>
